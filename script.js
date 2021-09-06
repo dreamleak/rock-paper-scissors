9dae0a147236356c5aa38d94f883f991af218efd
dreamleak <overhxul@gmail.com>
2021-09-06 19:52:39 -0400

    Implement scoreboard that simultaneously increments with met conditions

--- script.js
+++ script.js
@@ -1,72 +1,77 @@
-        // declared variables required to keep track of the score.
-        // let playerScore = parseInt(0),
-        //     compScore = parseInt(0);
+const playerSelectionDisplay = document.getElementById('player-choice');
+const computerSelectionDisplay = document.getElementById('computer-choice');
+const getResultsDisplay = document.getElementById('show-results');
+const possibleChoices = document.querySelectorAll('button');
+const playerScoreDisplay = document.querySelector('#player-points');
+const computerScoreDisplay = document.querySelector('#computer-points');
+let playerChoice;
+let computerChoice;
+let getResults;
+let playerScore = 0;
+let computerScore = 0;
+let gameOver = false;
+const winningScore = 5;
 
-        // compares scores to determine a winner.
-        // for (let i = 0; i < 5; i++) {
-        //     let playerSelection = prompt('Choose wisely: rock, paper, or scissors?');
-        //     const computerSelection = computerPlay();
-        //     console.log(playRound(playerSelection, computerSelection));
-        //     console.log("your score = " + playerScore);
-        //     console.log("Computer's score = " + compScore);
-        // };
 
-        const playerSelectionDisplay = document.getElementById('player-choice');
-        const computerSelectionDisplay = document.getElementById('computer-choice');
-        const getResultsDisplay = document.getElementById('result-scores');
-        const possibleChoices = document.querySelectorAll('button');
-        let playerChoice;
-        let computerChoice;
-        let getResults;
+function playRound() {
+    possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+        playerChoice = e.target.id;
+        playerSelectionDisplay.textContent = playerChoice;
 
-  function playRound () {
-        possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-            playerChoice = e.target.id;
-            playerSelectionDisplay.innerHTML = playerChoice;
-            computerPlay();
-            scoreBoard();
-        }));
-
-        function computerPlay() {
-            // automates computer's every move.
-            const randomValue = Math.floor(Math.random() * 3);
-
-            if (randomValue === 0) {
-                computerChoice = 'rock';
+        function scoreBoard() {
+            if (!gameOver && getResults === "You win!") {
+                playerScore++;
+                if (playerScore === winningScore) {
+                    gameOver = true;
+                } else if (getResults === "You lose! Computer wins!") {
+                    playerScore--;
+                } else {
+                    gameOver = false;
+                }
+                playerScoreDisplay.textContent = playerScore;
             }
-             else if (randomValue === 1) {
-                computerChoice = 'paper';
-            } else { 
-                computerChoice = 'scissors';
+        if (!gameOver && getResults === "You lose! Computer wins!") {
+            computerScore++;
+            if (computerScore === winningScore) {
+                gameOver = true;
+            } else if (getResults === "You win!") {
+                computerScore--;
+            } else {
+                gameOver = false;
             }
-            computerSelectionDisplay.textContent = computerChoice;
-     }
-
-    function scoreBoard() {
-        // still must add scores for each turn.
-        // playable without score tracker.
-      if (playerChoice === computerChoice) {
-            getResults = 'Draw! Rematch?';
-            }
-      if (playerChoice === 'paper' && computerChoice === 'rock') {
-            getResults = 'You win!';      
+            computerScoreDisplay.textContent = computerScore;
         }
-      if (playerChoice === 'paper' && computerChoice === 'scissors') {
+    }
+        computerPlay();
+        resultTracker();
+        scoreBoard();
+    }));
+
+    function computerPlay() {
+        // automates computer's every move.
+        const randomValue = Math.floor(Math.random() * 3);
+
+        if (randomValue === 0) {
+            computerChoice = 'rock';
+        } else if (randomValue === 1) {
+            computerChoice = 'paper';
+        } else {
+            computerChoice = 'scissors';
+        }
+        computerSelectionDisplay.textContent = computerChoice;
+    }
+
+    function resultTracker() {
+        if (playerChoice === computerChoice) {
+            getResults = 'Draw! Rematch?';
+        }
+        if ((playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'paper')) {
+            getResults = 'You win!';
+        }
+        if ((playerChoice === 'paper' && computerChoice === 'scissors') || (playerChoice === 'rock' && computerChoice === 'paper') || (playerChoice === 'scissors' && computerChoice === 'rock')) {
             getResults = 'You lose! Computer wins!';
-      } 
-      if (playerChoice === 'rock' && computerChoice === 'scissors') {
-          getResults = "You win!";
-      }
-      if (playerChoice === 'rock' && computerChoice === 'paper') {
-          getResults = 'You lose! Computer wins!';
-      }
-      if (playerChoice === 'scissors' && computerChoice === 'paper') {
-          getResults = 'You win!';
-      }
-      if (playerChoice === 'scissors' && computerChoice === 'rock') {
-          getResults = 'You lose! Computer wins!';
-      }
+        }
         getResultsDisplay.textContent = getResults;
     }
 }
-playRound();
+    playRound();
